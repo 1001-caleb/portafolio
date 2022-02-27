3b178b77fd1dcb1e59196738193b3d2b2dbca67b
Caleb Parrra <mccalebyt@gmail.com>
2022-02-27 14:39:24 -0500

    added scrollreveal

--- js/main.js
+++ js/main.js
@@ -25,3 +25,37 @@ menu.addEventListener("click", function(){
     nav.classList.toggle("nav__show")
     console.log("click")
 })
+
+// var config = {
+//     reset: true, // Al desplazar el mouse, el interruptor de animación (el valor predeterminado es falso sin activar el interruptor de animación para el desplazamiento del mouse)
+//              origin: 'right', // la dirección donde comienza la animación
+//              duración: 500, // duración de la animación
+//              retraso: 0, // retraso
+//              rotar: {x: 0, y: 0, z: 0}, // ángulo inicial de transición a 0
+//              opacidad: 0.2, // transparencia inicial (el efecto de 0,2 a 1)
+//              escala: 2, // Zoom
+//              easing: 'easy-in-out', // efecto de animación // easing'ease ',' easy-in-out ',' linear '...
+//              // Llamar de vuelta 
+//              // Se activará antes de que comience la animación
+//     beforeReveal: function(domEl){
+//                      console.log ('Animación ejecutada');
+//     },
+//              // Se activará antes de que la rueda del mouse se desplace
+//     beforeReset: function(domEl){
+//                      console.log ('La rueda arranca ---');
+//     },
+//              // Se activará después de que comience la animación
+//     afterReveal: function(domEl){
+//                      console.log ('La animación ha terminado');
+//     },
+//              // Se activará después de que se desplace la rueda de desplazamiento
+//     afterReset: function(domEl){
+//                      console.log ('La rueda se acabó');
+//     }
+// };
+
+window.sr = ScrollReveal();
+
+sr.reveal('.skill__img', {
+    duration: 3000
+});
